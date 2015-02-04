6641cd1857b989aca939a65694f3e75f9a4607b8
larseimermacher <lars.eimermacher@coupies.de>
2015-02-04 11:58:25 +0100

    delete unnecessary android files

--- coupies-framework_lib/src/main/java/de/coupies/framework/session/UserSession.java
+++
@@ -1,9 +0,0 @@
-package de.coupies.framework.session;
-
-import de.coupies.framework.beans.User;
-
-
-public interface UserSession extends CoupiesSession {
-	String getRememberKey();
-	User getUser();
-}
